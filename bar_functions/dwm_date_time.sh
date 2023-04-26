fc11d9fb04bc43e0e5e4acb4e723b3bbfdc213f3
unknown_source <letmego@example.com>
2023-04-26 12:39:48 -0400

    changes the the date and time formate. new format : WeekdayName MonthName-MM DD Hour:Min am/pm

--- bar_functions/dwm_date_time.sh
+++ bar_functions/dwm_date_time.sh
@@ -2,11 +2,11 @@
 
 # Shows the current date and time
 
-# Date format: "[WeekdayName MonthName YYYY-MM-DD Hour:Min:Sec]"
+# Date format: "[WeekdayName (MonthName-MM) DD Hour:Min am/pm]"
 dwm_date_time () {
     if [ "$USE_EMOJIES" = "true" ]; then
-        printf "📆 %s" "$(date "+%a %b %Y-%m-%d %T")"
+        printf "📆 %s" "$(date "+%a %d %b-%m %H:%M %P")"
     else
-        printf "D&T %s" "$(date "+%a %b %Y-%m-%d %T")"
+        printf "D&T %s" "$(date "+%a %d %b-%m %H:%M %P")"
     fi
 }
